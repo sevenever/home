abe1ae6f5630b6cb56b1f1aaa02fd4b2be5c1735
Feng Shao <sevenever@gmail.com>
2020-08-11 20:46:22 +0800

    move some alias to .bash_aliases

--- .bash.d/misc.sh
+++ .bash.d/misc.sh
@@ -1,6 +1,4 @@
 
-alias wolnas='python "/Users/shaofeng/mygithub/sevenever/sevenever/misc/wol.py" 192.168.1.255 53 f4:6d:04:db:27:ad'
-
 #sshforget(){
     #local host
     #local port
@@ -25,7 +23,3 @@ alias wolnas='python "/Users/shaofeng/mygithub/sevenever/sevenever/misc/wol.py" 
 genpw () { echo -n $1 | md5 | python3 -c 'import sys; print("".join(dict(zip("0123456789abcdef","0!2#$%^789@BCdef"))[c] for c in sys.stdin.read().strip()))'; }
 
 
-alias svcroot='ssh -p 26 -l root -i ~/.ssh/id_rsa.work -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
-alias svcscp='scp -P 26 -o User=root -i ~/.ssh/id_rsa.work -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
-# alias eclipse='/Users/shaofsh/Software/Eclipse.app/Contents/MacOS/eclipse &'
-alias eclipse='open -n /Applications/Eclipse.app'
