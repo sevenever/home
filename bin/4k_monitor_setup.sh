819908b23e26a5c8312b14368dc3fd0937e2dbc3
Feng Shao <sevenever@gmail.com>
2020-08-23 12:15:02 +0800

    remove bufferline

--- bin/4k_monitor_setup.sh
+++ bin/4k_monitor_setup.sh
@@ -18,7 +18,7 @@ case $1 in
     gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "[{'Gdk/WindowScalingFactor', <1>}]"
     ;;
     *)
-    xrandr --dpi 96 --fb 7680x2160 --output eDP --scale 2x2 --mode 1920x1080 --pos 3840x0 --output DisplayPort-0 --scale 1x1 --mode 3840x2160 --rate 60 --pos 0x0
+    xrandr --dpi 96 --fb 7680x2160 --output eDP --scale 2x2 --mode 1920x1080 --pos 3840x0 --output DisplayPort-0 --scale 0.9999x0.9999 --mode 3840x2160 --rate 60 --pos 0x0
     gsettings set org.gnome.desktop.interface scaling-factor 2
     gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "[{'Gdk/WindowScalingFactor', <2>}]"
 esac
