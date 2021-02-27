948f380ff39385e3611c0a4dd6c93a430a26860a
Feng Shao <sevenever@gmail.com>
2021-02-27 18:26:35 +0800

    DELL

--- bin/4k_monitor_setup.sh
+++ bin/4k_monitor_setup.sh
@@ -3,26 +3,19 @@
 case $1 in
     4k)
     xrandr --dpi 96 --fb 4800x4860 --output eDP --scale 2x2 --mode 1920x1080 --pos 0x2700 --output DisplayPort-0 --scale 1.25x1.25 --mode 3840x2160 --rate 60 --pos 0x0
-    #gsettings set org.gnome.desktop.interface scaling-factor 2
-    #gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "[{'Gdk/WindowScalingFactor', <2>}]"
     ;;
     1k)
     xrandr --dpi 96 --fb 3840x2160 --output eDP --scale 2x2 --mode 1920x1080 --pos 0x0
-    #gsettings set org.gnome.desktop.interface scaling-factor 1
-    #gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "[{'Gdk/WindowScalingFactor', <1>}]"
     ;;
     off)
     xrandr --dpi 96 --fb 3840x2160 --output eDP --scale 2x2 --mode 1920x1080 --pos 0x0 --output DisplayPort-0 --off
-    #gsettings set org.gnome.desktop.interface scaling-factor 1
-    #gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "[{'Gdk/WindowScalingFactor', <1>}]"
+    ;;
+    l)
+    xrandr --dpi 96 --fb 3840x4320 --output eDP --scale 2x2 --mode 1920x1080 --pos 0x2160 --output DisplayPort-0 --scale 0.9999x0.9999 --mode 3840x2160 --rate 60 --pos 0x0
     ;;
     v)
-    xrandr --dpi 96 --fb 3840x4320 --output eDP --scale 2x2 --mode 1920x1080 --pos 0x2160 --output DisplayPort-0 --scale 0.9999x0.9999 --mode 3840x2160 --rate 60 --pos 0x0
-    #gsettings set org.gnome.desktop.interface scaling-factor 2
-    #gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "[{'Gdk/WindowScalingFactor', <2>}]"
+    xrandr --dpi 96 --fb 6000x3840 --output eDP --scale 2x2 --mode 1920x1080 --pos 0x1680 --output DisplayPort-0 --scale 0.9999x0.9999 --mode 3840x2160 --rate 60 --pos 3840x0
     ;;
     *)
     xrandr --dpi 96 --fb 7680x2160 --output eDP --scale 2x2 --mode 1920x1080 --pos 3840x0 --output DisplayPort-0 --scale 0.9999x0.9999 --mode 3840x2160 --rate 60 --pos 0x0
-    #gsettings set org.gnome.desktop.interface scaling-factor 2
-    #gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "[{'Gdk/WindowScalingFactor', <2>}]"
 esac
--- bin/setup_monitors.bash
+++ bin/setup_monitors.bash
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-num_monitors=$(xrandr --listmonitors | head -1 | cut -d ':' -f 2 | tr -d ' ')
+external_monitor=''
 
-if [[ "${num_monitors}" == "2" ]];then
-    4k_monitor_setup.sh v
-elif [[ "${num_monitors}" == "1" ]];then
-    4k_monitor_setup.sh 1k
+hwinfo=$(hwinfo --monitor --short)
+
+echo ${hwinfo} | grep 'DELL P2415Q' > /dev/null
+if [[ $? == 0 ]];then
+    external_monitor='DELL'
 fi
+echo ${hwinfo} | grep 'LG ELECTRONICS LG Ultra HD' > /dev/null
+if [[ $? == 0 ]];then
+    external_monitor='LG'
+fi
+
+case ${external_monitor} in
+    LG)
+        4k_monitor_setup.sh l
+        ;;
+    DELL)
+        4k_monitor_setup.sh v
+        ;;
+    *)
+        4k_monitor_setup.sh 1k
+esac
